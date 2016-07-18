6a896b844cd974392e7a9f53083cb0e3fe6f1f86
aizr1 <aizr1@yahoo.de>
2016-07-18 13:16:33 +0200

    working edits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,30 @@ package com.company;
  * Created by theo on 16.07.16.
  */
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 import static java.lang.System.out;
 
 public class Main {
 
-    private static long p,q,c,message;
+    //GLOBALE VARIABLEN
+    private static long
+            p, //PRIMZAHL 1
+            q, //PRIMZAHL 2
+            c, //VERSCHLÜSSELUNGSSCHLÜSSEL
+            m, //GANZZAHLINGE NACHRICHT
+            vm, //VERSSCHLÜSSELTE NACHRICHT
+            d, //ENTSCHLÜSSELUNGSSCHLÜSSEL
+            phi, //PHI
+            n; //
+
 
     public static void main(String[] args) {
 
-
-        //Eingabe von P und Q
+        //Eingabe von P und Q sowie Überprüfung ob gultige Primzahl
         out.println("Bitte erste Primzahl p eingeben.");
         Scanner pscan = new Scanner(System.in);
-        long p = pscan.nextLong();
+        p = pscan.nextLong();
 
         if (isPrim(p)) {
             out.println("P ist eine Primzahl");
@@ -30,7 +38,7 @@ public class Main {
 
         out.println("Bitte erste Primzahl q eingeben.");
         Scanner qscan = new Scanner(System.in);
-        long q = qscan.nextLong();
+        q = qscan.nextLong();
         if (isPrim(q)) {
             out.println("Q ist eine Primzahl");
         } else {
@@ -43,24 +51,25 @@ public class Main {
         }
 
         // n berechnen
-        long n = p * q;
+        n = p * q;
         System.out.println("Testausgabe: n = p * q = " + n);
 
         //phi berechnen
-        System.out.println("Testausgabe: phi(n)=(p-1)*(q-1) = " + phi(p, q));
+        phi = phi(p,q);
+        System.out.println("Testausgabe: phi(n)=(p-1)*(q-1) = " + phi);
 
         //Eingabe von der Nachricht M
         System.out.println("Bitte Nachricht m eingeben.");
         Scanner mscan = new Scanner(System.in);
-        long message = mscan.nextLong();
+        m = mscan.nextLong();
 
         //Eingabe des Verschlüsselungsschlüssel
         System.out.println("Bitte Verschlüsselungsschlüssel c eingeben.");
         Scanner cscan = new Scanner(System.in);
-        long c = cscan.nextLong();
+        c = cscan.nextLong();
 
-        if (3 <= c && c < phi(p, q)) {
-            if (isTeilerfremd(c, phi(p, q))) {
+        if (3 <= c && c < phi) {
+            if (isTeilerfremd(c, phi)) {
 
                 System.out.println("Verschlüsselungsschlüssel kann verwendet werden, da 3 <= C < PHI und C, PHI teilerfremd sind.");
 
@@ -73,13 +82,14 @@ public class Main {
 
         //Verschlüsselung
         System.out.println("Verschlüsselte Nachricht:");
-        System.out.println(crypt(message,c,n));
+        System.out.println(crypt(m,c,n));
+        vm = crypt(m,c,n);
+        System.out.println("vm = " + vm);
 
         //Entschlüsselung
         System.out.println("Entschlüsselte Nachricht");
-        System.out.println(decrypt(message,getDecryptKey(),n));
+        System.out.println(decrypt(vm,n));
     }
-
     private static boolean isPrim(final long value) {
         if (value <= 2) {
             return (value == 2);
@@ -90,14 +100,6 @@ public class Main {
             }
         }
         return true;
-    }
-
-    private static long potenz(long basis, long exponent) {
-        long x = basis;
-        for (int j = 1; j < exponent; j++) {
-            x = basis;
-        }
-        return x;
     }
 
     private static boolean isTeilerfremd(long a, long b){
@@ -117,52 +119,35 @@ public class Main {
         return (q - 1) * (p - 1);
     }
 
-    private static long crypt(long message, long cryptKey, long n){
+    private static long crypt(long m, long c, long n){
         /*
         v(m) = m^c mod n
         v = m^c mod n
          */
-        long v = 0;
-        for (int i = 1; i <= cryptKey; i++){ //MOD besonders oft auf v anweden
-           v = potenz(message, cryptKey) % n;
+        long v = 1;
+        long x = m % n;
+        for (int i = 1; i <= c; i++){ //MOD besonders oft auf v anweden
+           v = v*x;
         }
+        v = v%n;
         return v;
     }
 
-    private static long decrypt(long message, long decryptKey, long n){
-        long e = 0;
-        for (int i = 1; i <= decryptKey; i++){
-            e = crypt(message,decryptKey,n) * decryptKey  % n;
-        }
-        return e;
-    }
+    private static long decrypt(long vm, long n){
 
-    private static long getDecryptKey(){
-        long d = 1;
-        /*
-        Was läuft hier falsch?
+        while (c * d % phi != 1)    //Entschlüsselungsschlüssel
+        {
+            d=d+1;
+            System.out.print(d+", ");
+        }
+        System.out.println();
+        System.out.println("Testoutput: vm = " + vm + " d = " + d + " n = " + n + " m = " + m);
 
-        for (;true;d++){
-            System.out.println(d);
-            System.out.println("Entschlüsselungsschlüssel generieren...");
-            if ((c*d)%phi(p,q) != 1) {
-                break;
-            }
+        long m = 1;
+        for (long i = 1; i < (d+1); i++){
+            m = (m * vm) % n;
+            System.out.print(m+", ");
         }
-        */
-
-        /*
-        while ((c*d)%phi(p,q) != 1){
-            System.out.println("Generieren... " + d);
-            d++;
-        }
-        */
-        for (;true;d++) {
-            System.out.println("Generieren... " + d);
-            if ((c*d)%phi(p,q) != 1) {
-                break;
-            }
-        }
-        return d;
+        return m;
     }
 }
